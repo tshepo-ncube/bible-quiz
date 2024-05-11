c10a01d55979ab9678311085bf2a3cf162aa4145
tshepo-ncube <m.tshepo.ncube@gmail.com>
2024-05-11 17:09:29 +0200

    sddsd___ssdd8m2323

--- components/QuizComponent.jsx
+++ components/QuizComponent.jsx
@@ -54,7 +54,7 @@ export default function QuizComponent({
           handleTimeup();
           decrementQuestion();
           // Wait for 2 seconds before executing the delayedFunction
-          setTimeout(newQuestionShow, 2000);
+          setTimeout(newQuestionShow, 6000);
         }
         if (prevProgress <= 0) {
           clearInterval(id);
